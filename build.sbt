745376e7f2ea8b9ba1c959cf8af7875d6e494af8
Oli200110 <tbouwt@inf.elte.hu>
2023-02-01 10:39:21 +0100

    Crawler now fetches all distinct links from page

--- build.sbt
+++ build.sbt
@@ -10,3 +10,4 @@ lazy val root = (project in file("."))
 libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "4.8.0"
 libraryDependencies += "io.github.bonigarcia" % "webdrivermanager" % "5.3.2"
 libraryDependencies += "org.slf4j" % "slf4j-simple" % "2.0.5"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.4"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,44 @@
-import org.openqa.selenium.Capabilities
-import org.openqa.selenium.chromium.ChromiumOptions
+import org.openqa.selenium.By
 import org.openqa.selenium.remote.{DesiredCapabilities, RemoteWebDriver}
-
 import java.net.URL
+import scala.collection.mutable
+import scala.collection.parallel.CollectionConverters.seqIsParallelizable
 
 
 object Main {
   private val HUB_URL = "http://localhost:4444/wd/hub"
+  private val NUM_OF_DRIVERS = 2
+  private val START_URL = "https://www.telex.hu"
 
   def main(args: Array[String]): Unit = {
     println("Hello world!")
-    val chromiumOptions = new DesiredCapabilities()
-    chromiumOptions.setBrowserName("chromium")
-    val driver: RemoteWebDriver = new RemoteWebDriver(new URL(HUB_URL), chromiumOptions)
-    driver.get("https://www.google.com")
+    val desiredCapabilities = new DesiredCapabilities()
+    desiredCapabilities.setBrowserName("chrome")
+    val driver: RemoteWebDriver = new RemoteWebDriver(new URL(HUB_URL), desiredCapabilities)
+    driver.get(START_URL)
+
+    val linkPool = new mutable.ArrayDeque[String]()
+    linkPool += START_URL
+
+    linkPool.par.foreach(link => {
+      println(link)
+      if (link != null) {
+        println("--------")
+        driver.get(link)
+        driver
+          .findElements(By.tagName("a"))
+          .forEach(elem => {
+            val href: String = elem.getAttribute("href")
+            if (!linkPool.contains(href)) {
+              println(href)
+              linkPool.addOne(href)
+            }
+          })
+      }
+    })
+
+
     driver.quit()
+    println(linkPool.length)
   }
 }
