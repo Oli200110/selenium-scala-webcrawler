437888847566c83b8dd15615f6a28a94d18b0fc5
Oli200110 <tbouwt@inf.elte.hu>
2023-02-03 10:00:08 +0100

    Level-two nested crawling.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,44 +1,49 @@
+import CrawlDriver.dispatchDriver
 import org.openqa.selenium.By
 import org.openqa.selenium.remote.{DesiredCapabilities, RemoteWebDriver}
+
 import java.net.URL
 import scala.collection.mutable
 import scala.collection.parallel.CollectionConverters.seqIsParallelizable
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
 
 
 object Main {
   private val HUB_URL = "http://localhost:4444/wd/hub"
   private val NUM_OF_DRIVERS = 2
+  private val MAX_POOL_SIZE = 1000
   private val START_URL = "https://www.telex.hu"
 
   def main(args: Array[String]): Unit = {
     println("Hello world!")
     val desiredCapabilities = new DesiredCapabilities()
     desiredCapabilities.setBrowserName("chrome")
-    val driver: RemoteWebDriver = new RemoteWebDriver(new URL(HUB_URL), desiredCapabilities)
-    driver.get(START_URL)
+    val drivers = Array.fill(NUM_OF_DRIVERS) {
+      new RemoteWebDriver(new URL(HUB_URL), desiredCapabilities)
+    }
 
     val linkPool = new mutable.ArrayDeque[String]()
     linkPool += START_URL
 
-    linkPool.par.foreach(link => {
-      println(link)
-      if (link != null) {
-        println("--------")
-        driver.get(link)
-        driver
-          .findElements(By.tagName("a"))
-          .forEach(elem => {
-            val href: String = elem.getAttribute("href")
-            if (!linkPool.contains(href)) {
-              println(href)
-              linkPool.addOne(href)
-            }
-          })
-      }
-    })
+    val fg = {
+      for {
+        res <- dispatchDriver(drivers(0), linkPool)
+        result <- dispatchDriver(drivers(0), res)
+        _ <- Future {
+          println(result)
+        }
+      } yield ()
+    }
 
 
-    driver.quit()
-    println(linkPool.length)
+    Await.result(fg, Duration.Inf)
+    println(fg)
+    //driver.quit()
+    //drivers.foreach(driver => driver.quit())
+    //println(linkPool.toSeq.length)
+
   }
 }
